b42a0d444f4110472ff131c21c1d886e216a0fc2
jfjiang <214055365@qq.com>
2017-03-15 09:45:04 +0800

    创建工程

---
+++ src/main/java/com/tunnel/qydev/mifeng/pattern/PatternLearning/App.java
@@ -0,0 +1,13 @@
+package com.tunnel.qydev.mifeng.pattern.PatternLearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
